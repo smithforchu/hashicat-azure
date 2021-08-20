723fbf7f734b0204a7686a9edc93f6c620864521
smithforchu <Smith.Forchu@fisglobal.com>
2021-08-20 19:19:27 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -116,6 +116,9 @@ resource "azurerm_virtual_machine" "catapp" {
     offer     = var.image_offer
     sku       = var.image_sku
     version   = var.image_version
+    tags = {
+    Billable = "true"
+  }
   }
 
   storage_os_disk {
